1b11bc2654e08fabbcf85e1a2f2b7a6c509f9c18
Ralph Largo <devlargs@gmail.com>
2022-02-06 12:46:02 +0800

    adjusted api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
+import client from "../utils/apolloClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 export default MyApp;
---
+++ pages/ships.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Ships = () => {
+  return (
+    <>
+      <Box>hehe</Box>
+    </>
+  );
+};
+
+export default Ships;
